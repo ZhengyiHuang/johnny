075c8764857caf5664c0f6a977b9050bcf87ef05
Johnny Huang <Zhuang82@sheffield.ac.uk>
2021-05-18 11:09:19 +0100

    update test module

--- ScrollBar.py
+++ ScrollBar.py
@@ -1,14 +1,26 @@
 import sys
-from PySide6 import QtGui, QtCore
-from PySide6.QtGui import QColor
-from PySide6.QtWidgets import QWidget, QComboBox, QApplication, QScrollBar
+
+from sys import exit
+
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QWidget, QScrollBar
+from PySide6.QtWidgets import QMenuBar, QMenu
+from PySide6.QtWidgets import QToolBar, QStatusBar
+from PySide6.QtWidgets import QFileDialog, QMessageBox
+
+from PySide6.QtCore import Qt, QPointF, QRectF
+from PySide6.QtGui import QColor, QPainter, QPainterPath, QPen, QBrush, QIcon
+from PySide6.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsEllipseItem, QGraphicsPathItem, \
+    QGraphicsSimpleTextItem, QGraphicsRectItem, QGraphicsItemGroup
+
+from PySide6.QtGui import QFontMetrics
 
 
+# ******************************************************************************
 class Window(QWidget):
-
     def __init__(self):
-        super(Window, self).__init__()
-        self.setWindowTitle("Johnny's Test")
+        super().__init__()
+        self.setWindowTitle("Johnny's Test Box")
         self.resize(500, 500)
         self.initUI()
 
@@ -16,7 +28,8 @@ class Window(QWidget):
         sb = QScrollBar(self)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Window()
-    ex.show()
+if __name__ == "__main__":
+    application = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    exit(application.exec_())
